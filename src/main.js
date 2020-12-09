ab8098221d3f7b7591f276e57218aca50b7cd031
felipelopezortega <flopezortega@ucol.mx>
2020-12-08 18:17:53 -0600

    second function added

--- src/main.js
+++ src/main.js
@@ -12,6 +12,29 @@ export default class App {
 
     }
 
+    sumatoriaSerieDos(numero2){
+
+        let suma = 1
+        let sumap
+        let sumar
+        let sumatotal 
+
+        for(let i=2; i<=numero2 ; i = i + 1){
+
+            if(i%2!=0){
+
+                sumar = suma -1/i
+            }else{
+
+                sumap= suma + 1/i
+            }
+                
+            sumatotal= sumap + sumar
+        }
+         return suma
+
+    }
+
 
 
 
@@ -25,3 +48,6 @@ let app = new App();
 
 //Función que reciba como parámetro un número y calcule la suma de la siguiente serie: 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/numero
 console.log(app.sumatoriaSerieUno(6))
+
+//Función dos
+console.log(app.sumatoriaSerieDos(6))
