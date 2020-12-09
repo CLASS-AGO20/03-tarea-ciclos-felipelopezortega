52f1cb1d29e73a76a39c939c4e6266bb25f17d2b
felipelopezortega <flopezortega@ucol.mx>
2020-12-08 18:06:38 -0600

    first function added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+
+        let suma = 1
+
+        for(let i=2; i<=numero ; i = i + 1){
+
+            suma = suma + 1/i
+        }
+         return suma
+
+    }
+
+
+
+
+
+
+
+
+}
 
 let app = new App();
+
+//Función que reciba como parámetro un número y calcule la suma de la siguiente serie: 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/numero
+console.log(app.sumatoriaSerieUno(6))
